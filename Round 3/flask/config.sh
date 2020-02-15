16e5ea5a9cb2b80aa744473f46f7704b0615376b
Sam-mint-virtual <>
2020-02-16 00:02:32 +1300

    test2

---
+++ Round 3/flask/config.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cd `dirname "$0"` #use the dir that the script is in
+echo "Using dir `pwd`"
+rm -rf venv
+before=`which python`
+python3 -m venv venv
+. venv/bin/activate
+after=`which python`
+if [ "$before" == "$after" ]; then
+    echo "Couldn't setup python venv, is venv installed?"
+    exit
+else
+    echo "Python env changed"
+fi
+echo "Installing requirements"
+echo
+sleep 1
+pip install -r requirements.txt
+sleep 1
+echo
+echo "Done"
--- Round 3/flask/tiger/app.py
+++ Round 3/flask/tiger/app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from flask import Flask, redirect, render_template
+import auth
 app = Flask(__name__)
 
 @app.route('/')
@@ -17,6 +18,22 @@ def page_names_blank():
 @app.route('/names/<name>')
 def page_names(name):
     return render_template('names.html', name=name)
+
+@app.route('/admin/')
+def page_admin_redirect:
+    return redirect('/admin/login')
+
+@app.route('/admin/<req_page>')
+def page_admin(req_page):
+    
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "404 page not found", 404
+
+@app.errorhandler(403)
+def page_forbidden(e):
+    return "403 Get outta here boi", 403
 
 if __name__ == "__main__":
     app.run(debug=True)
