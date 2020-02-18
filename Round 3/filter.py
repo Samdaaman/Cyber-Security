373e7be342c6965c9e0b8e956b9710e1a1cd36ca
Sam-mint-virtual <>
2020-02-19 03:50:09 +1300

    test

--- Round 3/filter.py
+++ Round 3/filter.py
@@ -4,6 +4,6 @@ aA1_ne = '():?_-=+ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 def filter(object, level=3):
     if (object.__class__ == list or object.__class__ == tuple):
-        for each item
+        for each item:
 
 filter('')
