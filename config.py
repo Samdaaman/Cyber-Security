3dd0aba5526a9ef2408052c2c196bc0945872922
Sam-virtual-mint <>
2020-05-26 02:52:55 +1200

    updated for linux

--- config.py
+++ config.py
@@ -6,15 +6,15 @@ os.system("python3 -m venv venv")
 if os.path.isdir("venv"):
     if os.path.isdir("venv/Scripts"):
         # windows
-        os.system("venv\\Scripts\\pip.exe install -r requirements.txt")
+        os.system("venv/Scripts/pip.exe install -r requirements.txt")
         print('\n\nConfigured for windows. Activating venv...')
-        os.system("cmd.exe /K venv\\Scripts\\activate.bat")
+        os.system("cmd.exe /K venv/Scripts/activate.bat")
 
     elif os.path.isdir("venv/bin"):
         # linux
-        os.system("venv\\bin\\pip.exe install -r requirements.txt")
+        os.system("venv/bin/pip install -r requirements.txt")
         print('\n\nConfigured for linux. Activating venv...')
-        os.system("venv\\bin\\activate")
+        os.system("/bin/bash --rcfile venv/bin/activate")
 
     else:
         print('What version of venv is this, missing sub dirs')
