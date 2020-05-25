04af6c9b47a0454f4a88e5fe8718c3b318494b87
Sam-virtual-mint <>
2020-05-26 03:34:50 +1200

    updated ready for test

--- setup_linux_runonce.sh
+++ setup_linux_runonce.sh
@@ -1,2 +1,6 @@
-sudo apt install python3-venv python3-pip git binwalk
-wget "https://portswigger.net/burp/releases/download?product=community&type=Linux"
+git clone git@github.com/Samdaaman/Cyber-Security.git
+sudo apt install -y python3-venv python3-pip git binwalk wireshark
+wget "https://portswigger.net/burp/releases/download?product=community&type=Linux" -o burp.sh
+chmod +x burp.sh
+./burp.sh
+
