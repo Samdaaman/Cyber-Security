3128eccf77dcfed87bf04e74436a158d09277cbf
Sam <50023329+Samdaaman@users.noreply.github.com>
2020-05-26 11:33:30 +1200

    added pycharm

--- setup_linux_runonce.sh
+++ setup_linux_runonce.sh
@@ -1,6 +1,7 @@
 git clone git@github.com:Samdaaman/Cyber-Security.git
 sudo apt install -y python3-venv python3-pip git binwalk wireshark
 python3 Cyber-Security/setup.py no-shell
+wget --no-cache https://download-cf.jetbrains.com/python/pycharm-community-2020.1.1.tar.gz
 wget --no-cache -O burp.sh "https://portswigger.net/burp/releases/download?product=community&type=Linux"
 chmod +x burp.sh
 ./burp.sh
