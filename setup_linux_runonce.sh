db235e9e32c886a024c8442675f46547e6ea816b
Sam <50023329+Samdaaman@users.noreply.github.com>
2020-05-26 03:49:54 +1200

    add temp deletion and running setup

--- setup_linux_runonce.sh
+++ setup_linux_runonce.sh
@@ -1,6 +1,8 @@
 git clone git@github.com:Samdaaman/Cyber-Security.git
 sudo apt install -y python3-venv python3-pip git binwalk wireshark
+python3 Cyber-Security/setup.py no-shell
 wget --no-cache -O burp.sh "https://portswigger.net/burp/releases/download?product=community&type=Linux"
 chmod +x burp.sh
 ./burp.sh
-
+rm burp.sh
+rm -- "$0"
