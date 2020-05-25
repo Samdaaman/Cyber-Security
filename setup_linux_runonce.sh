63be654fcd6197631661e6739daeaf26e9523ba6
Sam <50023329+Samdaaman@users.noreply.github.com>
2020-05-26 03:39:53 +1200

    added --no-cache for wget

--- setup_linux_runonce.sh
+++ setup_linux_runonce.sh
@@ -1,6 +1,6 @@
 git clone git@github.com:Samdaaman/Cyber-Security.git
 sudo apt install -y python3-venv python3-pip git binwalk wireshark
-wget -O burp.sh "https://portswigger.net/burp/releases/download?product=community&type=Linux"
+wget --no-cache -O burp.sh "https://portswigger.net/burp/releases/download?product=community&type=Linux"
 chmod +x burp.sh
 ./burp.sh
 
