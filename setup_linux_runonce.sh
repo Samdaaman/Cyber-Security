193096d32efebb66cb552609f0fd3a232e4bdb16
Sam <50023329+Samdaaman@users.noreply.github.com>
2020-05-26 03:37:23 +1200

    minor bug fixes

--- setup_linux_runonce.sh
+++ setup_linux_runonce.sh
@@ -1,6 +1,6 @@
-git clone git@github.com/Samdaaman/Cyber-Security.git
+git clone git@github.com:Samdaaman/Cyber-Security.git
 sudo apt install -y python3-venv python3-pip git binwalk wireshark
-wget "https://portswigger.net/burp/releases/download?product=community&type=Linux" -o burp.sh
+wget -O burp.sh "https://portswigger.net/burp/releases/download?product=community&type=Linux"
 chmod +x burp.sh
 ./burp.sh
 
